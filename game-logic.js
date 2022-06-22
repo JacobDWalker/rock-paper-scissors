2e1e601b0f0ab99cfb0bb593f6ddda2f79645539
JacobDWalker <jacobdanwalker@gmail.com>
2022-06-22 00:16:15 -0400

    Add 5 round system to game logic

--- game-logic.js
+++ game-logic.js
@@ -14,7 +14,7 @@ function userMove() {
     return move;
 };
 
-function playGame() {
+function playRound() {
     let userChoice = userMove();
     let computerChoice = getRandomInt();
 
@@ -23,9 +23,35 @@ function playGame() {
 
     if (userChoice === computerChoice)  {
         console.log("You Tie");
+        return "You Tie";
     } else if (((computerChoice + 1) % 3) === userChoice) {
         console.log("You Win");
+        return "You Win";
     } else {
         console.log("You lose");
+        return "You Lose"
     }
 };
+
+function playGame() {
+    let playerWins = 0;
+    let computerWins = 0;
+    for (let i = 0; i < 5; i++) {
+        let gameOutcome = playRound();
+
+        if (gameOutcome === "You Win") {
+            ++playerWins;
+        } else if (gameOutcome === "You Lose") {
+            ++computerWins;
+        }
+    }
+
+    if (playerWins > computerWins) {
+        console.log(`You won the game with ${playerWins} wins to ${computerWins}`)
+    } else if (playerWins === computerWins) {
+        console.log(`You with ${playerWins} wins`)
+    } else {
+        console.log(`You lost the game with ${playerWins} wins to ${computerWins}`)
+    }
+
+}
