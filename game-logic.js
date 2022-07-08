1169cb96cfc37104b569f53f71dfc2290a819c72
JacobDWalker <jacobdanwalker@gmail.com>
2022-07-07 23:00:35 -0400

    Create interactive UI for rock paper scissors game
    Previously, the RPS game logic was console based and relied on
    using devtools and alert messages to use. The game logic has been
    integrated into the UI of the web app. The users can now use the buttons
    to choose their move and see the status of the game, the round, and
    score all on the actual webpage.

--- game-logic.js
+++ game-logic.js
@@ -1,57 +1,119 @@
-const moves = ["rock", "paper", "scissors"]
+const moves = ["Rock", "Paper", "Scissors"]
 
-function computerMove() {
-    return moves[getRandomInt()];
 
-};
 function getRandomInt() {
-    let randomInt = Math.floor(Math.random() * 3)
-    return randomInt;
-};
+    return Math.floor(Math.random() * 3);
+}
 
-function userMove() {
-    let move = Number(prompt("Rock (0), Paper(1), or Scissors(2)"));
-    return move;
-};
 
-function playRound() {
-    let userChoice = userMove();
+
+function playRound(playerMove) {
     let computerChoice = getRandomInt();
+    let userChoice = Number(playerMove);
 
-    console.log(`Computer move: ${moves[computerChoice]}`);
-    console.log(`Your move: ${moves[userChoice]}`);
+    computerMoves.textContent = "Computer Move: ";
+    computerMoves.appendChild((selectMoveIcon(computerChoice)).cloneNode());
+    userMoves.textContent = "Your Move: ";
+    userMoves.appendChild((selectMoveIcon(userChoice)).cloneNode());
 
     if (userChoice === computerChoice)  {
-        console.log("You Tie");
-        return "You Tie";
+        gameResultDiv.textContent = "You Tied";
     } else if (((computerChoice + 1) % 3) === userChoice) {
-        console.log("You Win");
-        return "You Win";
+        userScore++;
+        gameResultDiv.textContent = "You Won"
     } else {
-        console.log("You lose");
-        return "You Lose"
+        computerScore++;
+        gameResultDiv.textContent = "You Lost"
     }
-};
-
-function playGame() {
-    let playerWins = 0;
-    let computerWins = 0;
-    for (let i = 0; i < 5; i++) {
-        let gameOutcome = playRound();
-
-        if (gameOutcome === "You Win") {
-            ++playerWins;
-        } else if (gameOutcome === "You Lose") {
-            ++computerWins;
-        }
-    }
-
-    if (playerWins > computerWins) {
-        console.log(`You won the game with ${playerWins} wins to ${computerWins}`)
-    } else if (playerWins === computerWins) {
-        console.log(`You with ${playerWins} wins`)
-    } else {
-        console.log(`You lost the game with ${playerWins} wins to ${computerWins}`)
-    }
-
+    roundCount++;
+    updateScores();
 }
+
+function selectMoveIcon(move) {
+    if (move === 0) {
+        return rock;
+    } else if (move === 1) {
+        return paper;
+    } else {
+        return scissors;
+    }
+}
+
+function updateScores() {
+    roundTracker.textContent = roundCount.toString();
+    userScoreDiv.textContent = userScore.toString();
+    computerScoreDiv.textContent = computerScore.toString();
+}
+
+
+let userScore = 0;
+let computerScore = 0;
+let roundCount = 0;
+
+
+
+const iconsDiv = document.querySelector('.icons-body');
+const restartButton = document.querySelector('#restart-button');
+const playButton = document.querySelector('#play-button');
+
+iconsDiv.style.display = 'none';
+restartButton.style.display = 'none';
+
+// Divs to be updated after each round based on game outcome
+let userScoreDiv = document.getElementById('user-score');
+let computerScoreDiv = document.getElementById('computer-score');
+let roundTracker = document.getElementById('round-tracker');
+
+// Initializing scores and round count
+userScoreDiv.textContent = "0";
+computerScoreDiv.textContent = "0";
+roundTracker.textContent = "0";
+
+const moveIcons = document.querySelectorAll('.icon');
+
+// Elements that update after each round
+let gameResultDiv = document.getElementById('game-outcome');
+let computerMoves = document.getElementById('computer-moves');
+let userMoves = document.getElementById('user-moves');
+
+// Create smaller icons for move breakdown
+let rock = document.createElement("img");
+rock.src = '/image/rock.png';
+rock.alt = "rock"
+rock.width = 50;
+rock.height = 50;
+
+let paper = document.createElement("img");
+paper.src = '/image/paper.png';
+paper.alt = "paper"
+paper.width = 50;
+paper.height = 50;
+
+let scissors = document.createElement("img");
+scissors.src = '/image/scissors.png';
+scissors.alt = "scissors"
+scissors.width = 50;
+scissors.height = 50;
+
+
+
+
+moveIcons.forEach((icon) => icon.addEventListener('click', (e) =>  {
+    playRound(e.target.getAttribute('data-key'))
+}))
+
+playButton.addEventListener('click', () => {
+    iconsDiv.style.display = 'flex';
+    restartButton.style.display = 'flex';
+    playButton.style.display = 'none';
+})
+
+restartButton.addEventListener('click', () => {
+    userScore = 0;
+    roundCount = 0;
+    computerScore = 0;
+    updateScores();
+    gameResultDiv.textContent = "";
+    computerMoves.textContent = "";
+    userMoves.textContent = "";
+})
